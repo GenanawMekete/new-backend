10c65e5d6cb7d055f21fc79ec45e4f2d3053a069
GenanawMekete <104999951+GenanawMekete@users.noreply.github.com>
2025-11-23 03:44:35 +0300

    Create index.js

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Game = require('./Game');
+const BingoCard = require('./BingoCard');
+const Transaction = require('./Transaction');
+const Leaderboard = require('./Leaderboard');
+
+module.exports = {
+  User,
+  Game,
+  BingoCard,
+  Transaction,
+  Leaderboard
+};
